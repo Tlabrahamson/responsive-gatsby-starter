eea408b1e860dd811f7c5bc5a62510ee0e75f06a
Tim Abrahamson <tim@kinetic.com>
2019-12-13 10:56:03 -0600

    Made the logo a link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import { Link } from 'gatsby'
-import styles from '../css/navbar.module.css'
-import { FaBars } from 'react-icons/fa'
-import links from '../constants/links'
-import logo from '../images/logo.png'
+import React, { useState } from "react"
+import { Link } from "gatsby"
+import styles from "../css/navbar.module.css"
+import { FaBars } from "react-icons/fa"
+import links from "../constants/links"
+import logo from "../images/logo.png"
 
 const Navbar = () => {
   const [isOpen, setNav] = useState(false)
@@ -15,17 +15,19 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       <div className={styles.alignNav}>
         <div className={styles.navContent}>
-        {/* Website logo */}
-          <img src={logo} className={styles.logo} alt="your logo" />
+          <Link to={"/"}>
+            {/* Website logo */}
+            <img src={logo} className={styles.logo} alt="your logo" />
+          </Link>
           <button className={styles.hamburgerMenu} onClick={toggleNav}>
-          {/* Hamburger menu icon */}
+            {/* Hamburger menu icon */}
             <FaBars className={styles.hamburgerIcon} />
           </button>
         </div>
         <ul
           className={
             isOpen
-              ? `${styles.navLinks} ${styles.showNav}`
+              ? `${styles.navLinks} ${styles.showNav} `
               : `${styles.navLinks}`
           }
         >
